6f6fa1809b220d2daae3b328c2872dcd5850bca5
Barbara <barbara.h.kim@gmail.com>
2015-09-13 06:25:15 -0400

    shtuff

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
 	return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    adrs = request.form['address']
+    return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
